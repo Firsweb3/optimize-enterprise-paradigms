420c7258aa96cec6318e138f26d16da85771ba38
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-11-13 11:24:12 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,13 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+def main():
+
+def generate_random_data():
+import random
+    for item in data:
+        print(f"Random Number: {item}")
+    main()
+if __name__ == "__main__":
+    return data
+
