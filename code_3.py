e4c62f64e2887f2e619410e2f2e9a5918fb2268b
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-11-20 14:54:37 +0200

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,14 +1,61 @@
-    data = [random.randint(1, 100) for _ in range(10)]
+    data.forEach(item => {
+main();
+}
+function generateRandomData() {
 
-    main()
-        print(f"Random Number: {item}")
-    for item in data:
-    data = generate_random_data()
-    return data
-
-import random
-def main():
-if __name__ == "__main__":
+        console.log("Random Number: " + item);
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+    for (let i = 0; i < 10; i++) {
+}
+    const data = [];
+function main() {
 
 
-def generate_random_data():
+    });
+    return data;
+    const data = generateRandomData();
+    const data = [];
+        console.log("Random Number: " + item);
+    return data;
+main();
+    data.forEach(item => {
+function generateRandomData() {
+
+function main() {
+}
+    for (let i = 0; i < 10; i++) {
+    }
+    });
+
+    const data = generateRandomData();
+        data.push(Math.floor(Math.random() * 100) + 1);
+}    });
+}
+
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+    data.forEach(item => {
+    const data = [];
+        console.log("Random Number: " + item);
+function main() {
+    for (let i = 0; i < 10; i++) {
+main();
+    return data;
+    const data = generateRandomData();
+}
+function generateRandomData() {
+            System.out.println("Random Number: " + item);
+}
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+        }
+    }
+        int[] data = new int[10];
+    public static void main(String[] args) {
+
+
+        }
+        for (int i = 0; i < 10; i++) {
